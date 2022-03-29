3f42e99b783da0539bf37eb6b64d836271c89932
osher.sebbag <osher.sebbag@tikalk.com>
2022-03-29 12:30:34 +0300

    osher

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
 
-console.log("osher hamelech");
+console.log("osher hamelech!");
 
 app.get("/", async (req, res) => {
   res.json({ status: "all is fine" });
